0a8c10aecf259adfff0c134781e63f9cc0dc0239
arthur <aonsquared@gmail.com>
2015-07-03 22:53:57 +0100

    Switch to pygame frame buffer display for greeting. Todo: add date display, and logging of date/time to file.

--- nfc_logbook.py
+++ nfc_logbook.py
@@ -6,13 +6,22 @@ import pygame
 
 import os
 
+mifare = nxppy.Mifare()
+
+f = open("user_list.csv",'r')
+
+user_hash = {}
+
+for line in f:
+    user_hash[line.split(',')[0]] = line.split(',')[1].strip()
+
 def signal_term_handler(signum = None, frame = None):
-	sys.stderr.write("Terminated.\n")
-	f.close()
-	sys.exit(0)
+    sys.stderr.write("Terminated.\n")
+    f.close()
+    sys.exit(0)
 
 for sig in [signal.SIGTERM, signal.SIGINT, signal.SIGHUP, signal.SIGQUIT]: 
-	signal.signal(sig, signal_term_handler)
+    signal.signal(sig, signal_term_handler)
 
 class pyscope :
     screen = None;
@@ -58,16 +67,37 @@ class pyscope :
         "Destructor to make sure pygame shuts down, etc."
 
     def test(self):
+        uid = None
         # Get a font and use it render some text on a Surface.
         font = pygame.font.Font(None, 80)
-        # Render some text with a background color
-        text_surface = font.render('Channel 0',
-            True, (255, 255, 255))
-        # Blit the text
-        self.screen.blit(text_surface, (100, 30))
-        # Update the display
-        pygame.display.update()
-        time.sleep(10)
+        for i in range(30):
+            #clear the screen
+            self.screen.fill((0, 0, 255))     
+            # Render some text with a background color
+            timestr = time.strftime("%H:%M:%S", time.localtime())
+            text_surface = font.render(timestr,
+                True, (255, 255, 255))
+            # Blit the text
+            self.screen.blit(text_surface, (100, 30))
+            # Update the display
+            pygame.display.update()
+            try:
+                uid = mifare.select()
+            except nxppy.SelectError:
+                pass
+            if uid is not None:
+                #print "Card detected:", uid
+                if uid in user_hash.keys():
+                    text_surface = font.render("Hello",
+                        True, (255, 255, 255))
+                    self.screen.blit(text_surface, (140, 180))                	
+                    text_surface = font.render(user_hash[uid],
+                        True, (255, 255, 255))
+                    self.screen.blit(text_surface, (160, 240))
+                uid = None #reset uid to None
+                pygame.display.update()
+                time.sleep(1) #pause for 1 seconds
+            time.sleep(1)
         #get out of full screen
         #size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
         pygame.quit()
@@ -77,27 +107,5 @@ scope = pyscope()
 scope.test()
 # Wait 10 seconds
 
-mifare = nxppy.Mifare()
-uid = None
-
-f = open("user_list.csv",'r')
-
-user_hash = {}
-
-for line in f:
-	user_hash[line.split(',')[0]] = line.split(',')[1].strip()
-
-# Select the first available tag and return the UID
-while True:
-	try:
-		uid = mifare.select()
-	except nxppy.SelectError:
-		pass
-	if uid is not None:
-		print "Card detected:", uid
-		if uid in user_hash.keys():
-			print "hello ", user_hash[uid] 
-		uid = None #reset uid to None
-		time.sleep(2) #pause for 2 seconds
 
 
